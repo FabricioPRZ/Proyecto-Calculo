f70f08984036489cee2089f81ff4bd9ef57234a1
Fabricio PRZ <233394@ids.upchiapas.edu.mx>
2024-04-07 03:03:12 -0600

    Logica segunda formula

--- Proyecto_Calculo/main.cpp
+++ Proyecto_Calculo/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <cmath>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
 
-int main(int argc, char** argv) {
-	return 0;
+int main() {
+  // Declarar las variables
+  double a, u, derivada;
+
+  // Obtener los valores de a y u del usuario
+  cout << "Ingrese el valor de a: ";
+  cin >> a;
+  cout << "Ingrese el valor de u: ";
+  cin >> u;
+
+  // Calcular la derivada
+  derivada = exp(u * log(a)) * log(a) * u;
+
+  // Mostrar el resultado
+  cout << "La derivada de y = a^u con respecto a x es: " << derivada << endl;
+
+  return 0;
 }
